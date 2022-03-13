f62a6dd111184c9de9f0d9a9cad0de1ff0ca4d21
unknown <flaviuever@gmail.com>
2022-03-13 19:18:52 +0200

    Updates

---
+++ precache-manifest.588e20487c5a5d8ad4fb01aad32537dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8e8d865962dee9515225315a780902",
+    "url": "/authentication-form/index.html"
+  },
+  {
+    "revision": "e62e9a08eb51549794a0",
+    "url": "/authentication-form/static/css/2.862b2f9f.chunk.css"
+  },
+  {
+    "revision": "e62e9a08eb51549794a0",
+    "url": "/authentication-form/static/js/2.7c93f880.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/authentication-form/static/js/2.7c93f880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583404aee369c52e7af4",
+    "url": "/authentication-form/static/js/main.59a3328b.chunk.js"
+  },
+  {
+    "revision": "a488752e1ec7e6bb89fe",
+    "url": "/authentication-form/static/js/runtime-main.75922885.js"
+  }
+]);
